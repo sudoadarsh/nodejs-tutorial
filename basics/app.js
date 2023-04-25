fe6c6e39a8aa3e067c9ba9ca34ee8bdb6862f236
sudoadarsh <sudoadarsh@gmail.com>
2023-04-25 17:55:55 +0530

    -- Routing in Node

--- basics/app.js
+++ basics/app.js
@@ -3,28 +3,38 @@
  */
 
 // Import supporting modules. To import personal modules, it should be prefixed with ./ (same folder, relative path) or /
-const http = require('http');
+const http = require("http");
 
 /**
- * To create a server. 
+ * To create a server.
  * It takes a request listener function as parameter.
  * [IncomingMessage] to receive messages.
  * [ServerResponse] to send response.
  */
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+  // Routing in Node.
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>NodeJS Tutorials</title></head>");
+    res.write(
+      '<body><form action="/welcome" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
-    // ServerResponse object can be used to set headers.
-    res.setHeader('Content-Type', 'text/html');
-    // Response can be send line by line.
-    res.write('<html>');
-    res.write('<head><title>NodeJS Tutorials</title></head>');
-    res.write('<body>NodeJS Writing responses</body>')
-    res.write('</html>');
-    // End the response.
-    res.end();
-})
+  // ServerResponse object can be used to set headers.
+  res.setHeader("Content-Type", "text/html");
+  // Response can be send line by line.
+  res.write("<html>");
+  res.write("<head><title>NodeJS Tutorials</title></head>");
+  res.write("<body>NodeJS Writing responses</body>");
+  res.write("</html>");
+  // End the response.
+  res.end();
+});
 
 /**
  * Listen to server.
