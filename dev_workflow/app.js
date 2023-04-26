abecd0675c1acadd75a5250d1c578bf84b18626d
sudoadarsh <sudoadarsh@gmail.com>
2023-04-26 11:20:33 +0530

    -- Adding third party packages in node.

---
+++ dev_workflow/app.js
@@ -0,0 +1,9 @@
+/**
+ * Using Node Package Manager.
+ * 
+ * Run [npm init] to setup the package.json file.
+ * Run [npm install --save] to add third party packages as production dependency.
+ * Run [npm install --save-dev] to add third party packages as development dependency.
+ * Run [npm install -g] to add the package to the machine so you can use it globally.
+ * Browse [npmjs.com] to get documentation of packages.
+ */
