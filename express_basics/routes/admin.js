4ae0738e3deea724fbbe9920a734fff87c83d780
sudoadarsh <sudoadarsh@gmail.com>
2023-05-04 10:50:11 +0530

    -- Added styling to Admin and shop pages.

--- express_basics/routes/admin.js
+++ express_basics/routes/admin.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+/**
+ * Util to get the root directory of the project.
+ */
+const rootDir = require('../utils/path');
 
 router.get('/addProduct', (request, response) => {
     // response.send('<form action="/admin/product" method="POST"><input type="text" name="title"></input><button type="submit">Submit</button></form>');
-    response.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    response.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/product', (request, response) => {
