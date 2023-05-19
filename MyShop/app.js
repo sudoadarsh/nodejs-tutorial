12a15b1b2646821e113500fc1413ef13854afea1
sudoadarsh <sudoadarsh@gmail.com>
2023-05-19 10:26:14 +0530

    -- app setup.

---
+++ MyShop/app.js
@@ -0,0 +1,43 @@
+/**
+ * Module imports.
+ */
+const express = require('express'); // express module.
+const bodyParse = require('body-parser'); // body-parser module.
+const path = require('path'); // path module.
+
+/**
+ * Local modules.
+ */
+const endpoints = require('./utils/endpoints'); // endpoints.
+const rootDir = require('./utils/root-dir'); // root directory.
+
+
+const app = express(); // Get an instance of express as app.
+
+/**
+ * Globally set the template engine.
+ */
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+/**
+ * Define the public files.
+ */
+app.use(express.static(path.join(rootDir, 'public')));
+
+/**
+ * Parses the incoming request.
+ */
+app.use(bodyParse.urlencoded({extended: false}));
+
+/**
+ * Handles request made with endpoint: /admin
+ */
+app.use(endpoints.admin);
+
+/**
+ * Handles request made with endpoint: /
+ */
+app.use(endpoints.home);
+
+app.listen(3000); // Start the server.
---
+++ MyShop/utils/endpoints.js
@@ -0,0 +1,10 @@
+/**
+ * The page contains all the avialble endpoints.
+ */
+const admin = 'admin';
+const home = '/';
+
+module.exports = {
+    "admin": admin,
+    "home": home
+}
