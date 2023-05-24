a07aecc71aa00d1316b963ddcf6498b37cf0afdb
sudoadarsh <sudoadarsh@gmail.com>
2023-05-24 10:27:59 +0530

    -- Dynamic routes starter project added.

--- MyShop/controllers/product.js
+++ MyShop/controllers/product.js
@@ -1,26 +1,26 @@
-const product = []; // The product list.
+const products = []; // The product list.
+const endpoints = require("../utils/endpoints"); // The endpoints util.
 
 const getProductList = (request, response) => {
-    response.render('shop/product-list', {
-        "title": "PRODUCTS LIST",
-        "stylePath": ""
-    });
-}
+  response.render("shop/product-list", {
+    title: "PRODUCTS LIST",
+    products: products
+  });
+};
 
 const getAddProduct = (request, response) => {
-    response.render('admin/add-product', {
-        "title": "ADD PRODUCTS",
-        "stylePath": ""
-    });
-}
+  response.render("admin/add-product", {
+    title: "ADD PRODUCTS",
+  });
+};
 
 const postAddProduct = (request, response) => {
-    product.push(request.body.title)
-    response.redirect(endpoints.homeAllProducts);
-}
+  products.push(request.body);
+  response.redirect(endpoints.homeAllProducts);
+};
 
 module.exports = {
-    "getProductList": getProductList,
-    "getAddProduct": getAddProduct,
-    "postAddProduct": postAddProduct
-}
+  getProductList: getProductList,
+  getAddProduct: getAddProduct,
+  postAddProduct: postAddProduct,
+};
